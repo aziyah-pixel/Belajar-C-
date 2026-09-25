43253e56924bc85d41bdc3f49325c9e2b21088f4
Nanda Nur Aziyah <aziyahnanda6@gmail.com>
2026-09-25 14:42:35 +0700

    konversi_suhu

---
+++ Konversi_suhu/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Konversi suhu dari celcius ke fahrenheit");
+Console.WriteLine("masukan suhu celcius nya");
+double celcius = Convert.ToDouble(Console.ReadLine());
+
+double fahrenheit = 9 * celcius / 5 + 32;
+
+Console.WriteLine("suhu fahrenheit nya adalah : " + fahrenheit);
+
+
